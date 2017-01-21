aed76f0a2d699a21be3989a038400e9326b56c9a
RyanFin <21296921@student.uwl.ac.uk>
2017-01-21 20:33:59 +0000

    Enemy class is now an abstract class
    • Once a class contains at least one pure virtual function (which has '= 0' syntax), which must be overriden by subclasses, it becomes an abstract class. Abstract classes cannot have objects of the same class type

--- Polly/main.cpp
+++ Polly/main.cpp
@@ -6,6 +6,9 @@ public:
 	virtual void attack(){
 		cout << "I am the enemy class" <<endl;
 	}
+
+	//Enemy class is now an abstract class with pure virtual function
+	virtual void move() = 0; //example of a pure virtual function, makes class an abstract class
 };
 
 class Ninja : public Enemy{
@@ -13,13 +16,22 @@ public:
 	void attack(){
 		cout << "Ninja Attack!" << endl;
 	}
+
+	//pure virtual function MUST be overrriden
+	void move(){
+		cout << "Ninja is moving" <<endl;
+	}
 };
 
 class Monster : public Enemy{
 public:
 	void attack(){
 		cout << "Monster Attaack!" << endl;
+	}
 
+	//pure virtual function MUST be overrriden
+	void move(){
+		cout << "Monster is moving" <<endl;
 	}
 	
 };
@@ -29,7 +41,8 @@ int main(){
 
 	Ninja n;
 	Monster m;
-	Enemy e;
+	//no Enemy objects. Once it is declared as an abstract class (by containing at least one virtual function) no objects can be instantiated
+	//Enemy e; 
 
 	Enemy* enemy1 = &n;
 	Enemy* enemy2 = &m;
@@ -37,6 +50,8 @@ int main(){
 
 	enemy1->attack();
 	enemy2->attack();
+	enemy1->move();
+	enemy2->move();
 	
 	
 
